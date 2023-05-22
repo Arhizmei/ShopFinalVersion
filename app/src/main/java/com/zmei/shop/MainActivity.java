fcebbf951c63c276e54230929b6c7e62aff35103
Arhizmei <arhizmei@gmail.com>
2023-05-22 16:30:02 +0300

    lessons36

--- app/src/main/java/com/zmei/shop/MainActivity.java
+++ app/src/main/java/com/zmei/shop/MainActivity.java
@@ -4,16 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     int quantity = 0;
+    Spinner mySpinner;
+    ArrayList mySpinnerArrayList;
+    ArrayAdapter mySpinnerAdapter;
+    HashMap  goodsMap;
+    String goodsName;
+    double price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    mySpinner = findViewById(R.id.mySpinner);
+    mySpinnerArrayList = new ArrayList();
+    mySpinnerArrayList.add("guitar");
+    mySpinnerArrayList.add("drums");
+    mySpinnerArrayList.add("keyboard");
+    mySpinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, mySpinnerArrayList);
+    mySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    mySpinner.setAdapter(mySpinnerAdapter);
+    goodsMap = new HashMap();
+    goodsMap.put("guitar", 100);
+    goodsMap.put("drums", 900);
+    goodsMap.put("keyboard", 600);
     }
 
     public void increaseQuantity(View view) {
@@ -33,4 +57,17 @@ public class MainActivity extends AppCompatActivity {
         quantitY.setText("" + quantity);
 
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        goodsName = mySpinner.getSelectedItem().toString();
+        price = goodsMap.get(goodsName);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
+
+
