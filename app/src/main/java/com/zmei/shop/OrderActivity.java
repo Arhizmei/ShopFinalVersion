8807b239b118a4b84225d711ef1209711a606dc4
Arhizmei <arhizmei@gmail.com>
2023-05-25 11:56:03 +0300

    newActivity

---
+++ app/src/main/java/com/zmei/shop/OrderActivity.java
@@ -0,0 +1,24 @@
+package com.zmei.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        setTitle("Zmei_order");
+        Intent orderIntent = getIntent();
+        String userName = orderIntent.getStringExtra("userName");
+        String goodsName = orderIntent.getStringExtra("goodsName");
+        int quantity = orderIntent.getIntExtra("quantity",0);
+        double orderPrice = orderIntent.getDoubleExtra("orderPrice",0);
+        TextView orderTextView = findViewById(R.id.orderTextView);
+        orderTextView.setText("Customer name: "+userName+ " \n" + "Goods name: "+goodsName+ " \n" + "Quantity: "+quantity+ " \n" +"Order price: "+orderPrice);
+    }
+}
